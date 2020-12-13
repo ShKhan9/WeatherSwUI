95579f83fcaaef9b0ffacafd978d988c6b75032c
Shehata Gamal <shehatagamal120$gmail.com>
2020-12-13 22:35:13 +0200

    Add imageLoader

---
+++ WeatherSwUI/Views/ImageLoader.swift
@@ -0,0 +1,29 @@
+//
+//  ImageLoader.swift
+//  WeatherSwUI
+//
+//  Created by MacBook Pro on 12/13/20.
+//  Copyright © 2020 MailMedia. All rights reserved.
+//
+
+import Foundation
+
+class ImageLoader: ObservableObject {
+    var didChange = PassthroughSubject<Data, Never>()
+    var data = Data() {
+        didSet {
+            didChange.send(data)
+        }
+    }
+
+    init(urlString:String) {
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                self.data = data
+            }
+        }
+        task.resume()
+    }
+}
