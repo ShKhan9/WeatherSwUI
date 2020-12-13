f969a5bea1125733144b2a16ed3f272c89f594c3
Shehata Gamal <shehatagamal120$gmail.com>
2020-12-13 22:35:40 +0200

    Add image Loader

--- WeatherSwUI/Views/MainListItem.swift
+++ WeatherSwUI/Views/MainListItem.swift
@@ -40,24 +40,3 @@ struct MainListItem: View {
       }
     }
 }
- 
-
-class ImageLoader: ObservableObject {
-    var didChange = PassthroughSubject<Data, Never>()
-    var data = Data() {
-        didSet {
-            didChange.send(data)
-        }
-    }
-
-    init(urlString:String) {
-        guard let url = URL(string: urlString) else { return }
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data else { return }
-            DispatchQueue.main.async {
-                self.data = data
-            }
-        }
-        task.resume()
-    }
-}
